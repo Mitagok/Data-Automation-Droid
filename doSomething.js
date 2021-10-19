f9431974fc6229734dafc6b4e5d45594b520bf17
Mitagok <mitagok@gmail.com>
2021-10-19 14:48:46 +0530

    Updated DoSomething.js
    Updated DoSomething.js

--- doSomething.js
+++ doSomething.js
@@ -1,5 +1,11 @@
-chrome.browserAction.onClicked.addListener((activeTab) =>
+/*chrome.browserAction.onClicked.addListener((activeTab) =>
 {
     chrome.windows.create({ url: chrome.runtime.getURL("hello.html"), type: 
+    "popup" });
+});*/
+
+chrome.browserAction.onClicked.addListener((activeTab) =>
+{
+    chrome.windows.create({ url: "https://www.google.com", type: 
     "popup" });
 });
